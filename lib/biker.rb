e2fe9952e30ab14a69bfaa0fee43744d2d370591
Davis Weimer <davisweimer@gmail.com>
2023-06-05 10:37:10 -0600

    Create learn_terrain, log_ride, and personal_record methods

--- lib/biker.rb
+++ lib/biker.rb
@@ -9,4 +9,21 @@ class Biker
     @rides = {}
     @acceptable_terrain = []
   end
+
+  def learn_terrain!(terrain_type)
+    
+    @acceptable_terrain.push(terrain_type)
+  end
+
+  def log_ride(ride_type, time )
+    if @rides[ride_type]
+      @rides[ride_type].push(time)
+    else
+      @rides[ride_type] = [time]
+    end
+  end
+
+  def personal_record(ride_type)
+    @rides[ride_type].min
+  end
 end
