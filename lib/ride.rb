9efcb86ae8ca4410501a1e8c028b7f5f50a99da5
Davis Weimer <davisweimer@gmail.com>
2023-06-05 09:29:29 -0600

    Create total_distance method

--- lib/ride.rb
+++ lib/ride.rb
@@ -6,8 +6,20 @@ class Ride
   def initialize(ride_traits)
     @name = ride_traits[:name]
     @distance = ride_traits[:distance]
-    @loop = false
+    @loop = ride_traits[:loop]
     @terrain = ride_traits[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    if @loop 
+      @distance
+    else
+      @distance *= 2
+    end
   end
 
 end
