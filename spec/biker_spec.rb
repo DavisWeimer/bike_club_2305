149ed70da401ea613a072ba556535cff375189b6
Davis Weimer <davisweimer@gmail.com>
2023-06-05 10:37:37 -0600

    Pass tests for learn_terrain, log_ride, and personal_record methods

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -17,4 +17,31 @@ RSpec.describe Biker do
       expect(@biker.acceptable_terrain).to eq([])
     end
   end
+
+  describe 'learn_terrain! method' do
+    it 'can add terrain' do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe 'log_ride and personal_record method' do
+    it 'can log all rides' do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+      expect(@biker.rides).to eq({@ride1=>[92.5, 91.1],@ride2=>[60.9, 61.6]})
+    end
+
+    it 'can log personal record for each ride' do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+      expect(@biker.personal_record(@ride1)).to eq(91.1)
+      expect(@biker.personal_record(@ride2)).to eq(60.9)
+    end
+  end
 end
