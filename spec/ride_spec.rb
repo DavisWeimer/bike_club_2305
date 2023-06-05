1cc0e4439ba52e435e06ac59caa1d6c33f83482d
Davis Weimer <davisweimer@gmail.com>
2023-06-05 11:48:17 -0600

    Remove Biker variable

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Ride do
   before :each do
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
-    @biker = Biker.new("Kenny", 30)
   end
   describe 'class' do
     it 'exists' do
